ccb3f6ef16e8f8ecab077ee44a25b72ad94339cb
maheshmudy <maheshplamudy7@gmail.com>
2023-02-27 00:10:41 +0530

    operational functions are addede in js file

---
+++ index.js
@@ -0,0 +1,26 @@
+function prime()
+{
+    var n=document.getElementById("number").value;
+    n=parseInt(n);
+    var check=parseInt(n);
+    switch(n)
+        {
+            case 1 : document.getElementById("result").value=" Not Prime Number";break;
+            case 2 : document.getElementById("result").value=" Prime Number";break;
+            default:for(let i=2;i<=n;i=i+1)
+              {
+                 if(check===i)
+                    {
+                    document.getElementById("result").value="Prime Number";
+                    break; 
+                    } 
+                 else  
+                 if(n%i===0)
+                    {
+                    document.getElementById("result").value=" Not Prime Number";
+                    break;
+                    }
+              } 
+        }
+        
+    }
